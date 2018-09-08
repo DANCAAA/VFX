15d89f4885aa0371ab43ebad4c4baf7c924c76e8
DANCAAA <josecastilloc93@hotmail.com>
2018-09-08 08:56:55 -0500

    Creating Proyect

---
+++ VFX/Assets/Scripts/AnimTime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTime : MonoBehaviour {
+
+    Animator mAnimator;
+
+	void Start () {
+        mAnimator = GetComponent<Animator>();
+
+    }
+	
+	void Update () {
+        AnimatorStateInfo state = mAnimator.GetCurrentAnimatorStateInfo(0);
+        float time = state.normalizedTime* state.length * state.speed;
+        Debug.Log(time);
+
+    }
+}
